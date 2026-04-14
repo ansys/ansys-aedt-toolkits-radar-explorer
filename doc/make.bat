b8067a8cb2d6f7fab86cc3f63b86a9487fc1d048
Samuelopez-ansys <samuel.lopez@ansys.com>
2026-04-14 16:16:56 +0200

    Fix make.bat

--- doc/make.bat
+++ doc/make.bat
@@ -7,21 +7,13 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
+if "%SPHINXOPTS%" == "" (
+	set SPHINXOPTS = -j auto -N -q
+)
 set SOURCEDIR=source
 set BUILDDIR=_build
 
 REM This LOCs are used to uninstall and install specific package(s) during CI/CD
-for /f %%i in ('pip freeze ^| findstr /c:"vtk-osmesa"') do set is_vtk_osmesa_installed=%%i
-if NOT "%is_vtk_osmesa_installed%" == "vtk-osmesa" if "%ON_CI%" == "true" (
-	@ECHO ON
-	echo "Removing vtk to avoid conflicts with vtk-osmesa"
-	@ECHO OFF
-	pip uninstall --yes vtk
-	@ECHO ON
-	echo "Installing vtk-osmesa"
-	@ECHO OFF
-	pip install --index-url https://wheels.vtk.org vtk-osmesa==9.3.1
-	)
 for /f %%i in ('pip freeze ^| findstr /c:"pypandoc_binary"') do set is_pypandoc_binary_installed=%%i
 if NOT "%is_pypandoc_binary_installed%" == "pypandoc_binary" if "%ON_CI%" == "true" (
 	@ECHO ON
@@ -31,8 +23,9 @@ if NOT "%is_pypandoc_binary_installed%" == "pypandoc_binary" if "%ON_CI%" == "tr
 	@ECHO ON
 	echo "Installing pypandoc-binary"
 	@ECHO OFF
-	pip install pypandoc-binary==1.13)
+	pip install pypandoc-binary==1.15)
 REM End of CICD dedicated setup
+
 
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
@@ -47,7 +40,7 @@ if errorlevel 9009 (
 	echo.may add the Sphinx directory to PATH.
 	echo.
 	echo.If you don't have Sphinx installed, grab it from
-	echo.http://sphinx-doc.org/
+	echo.https://sphinx-doc.org/
 	exit /b 1
 )
 
@@ -60,12 +53,14 @@ for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
 goto end
 
 :pdf
-echo Building PDF pages
 %SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 cd "%BUILDDIR%\latex"
 for %%f in (*.tex) do (
 xelatex "%%f" --interaction=nonstopmode)
-echo "Build finished. The PDF pages are in %BUILDDIR%."
+if NOT EXIST ansys-aedt-toolkits-radar-explorer.pdf (
+	Echo "no pdf generated!"
+	exit /b 1)
+Echo "pdf generated!"
 goto end
 
 :help
